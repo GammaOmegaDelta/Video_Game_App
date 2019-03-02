d3be194c6ce1e7b7c1e24d1c35393e28823d912e
Adrian <a.fernandez1@live.com>
2019-03-01 16:51:56 -0800

    added create

--- app/controllers/api/video_games_controller.rb
+++ app/controllers/api/video_games_controller.rb
@@ -9,4 +9,16 @@ class Api::VideoGamesController < ApplicationController
     @video_game = VideoGame.find_by(id: the_id)
     render 'show.json.jbuilder'
   end
+
+  def create
+    @video_game = VideoGame.new(
+      title: "Nier: Automata",
+      platform: "Playstation, PC, Xbox",
+      description: "Set in the midst of a proxy war between machines created by otherworldly invaders and the remnants of humanity, the story follows the battles of a combat android, her companion, and a fugitive prototype.",
+      players: 1,
+      genre: "Action Role-Play, Hack & Slash",
+      )
+    @video_game.savel
+    render 'show.json.jbuilder'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/video_games' => 'video_games#index'
-    get '/video_games/:id' => "video_games#show"
+    get '/video_games/:id' => 'video_games#show'
+    post '/video_games' => 'video_games#create'
   end
 end
